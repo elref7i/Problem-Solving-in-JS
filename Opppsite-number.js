9fe4637031c2c7fbb9285efc4dfd01b8458e5c0f
Ahmed re <momo3956248@gmail.com>
2024-09-08 12:12:06 +0300

    opposite Number

---
+++ Opppsite-number.js
@@ -0,0 +1,16 @@
+/*
+!!!
+Description:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 
+!!! 
+ */
+function opposite(number) {
+    return number * -1;
+}
+console.log(opposite(-10));
