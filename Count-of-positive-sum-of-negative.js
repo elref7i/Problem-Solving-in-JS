f4613fd8f3b2229c448fd8bb0e6144abd7261ea5
Ahmed re <momo3956248@gmail.com>
2024-09-09 15:51:57 +0300

    Count-of-positive-sum-of-negative

---
+++ Count-of-positive-sum-of-negative.js
@@ -0,0 +1,18 @@
+function countPositivesSumNegatives(input) {
+    let sum=0;
+    let count=0;
+    if(input.length === 0){
+        return null;
+    }
+    for (let i =0 ;i<input.length;i++){
+        if(input[i] >=0){
+            count ++;
+        }
+        else{
+            sum +=input[i];
+        }
+    }
+    return [count,sum];
+
+}
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]));
